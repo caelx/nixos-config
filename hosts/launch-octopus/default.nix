7d036df9e001e4c564461dba820f085d40deab08
caelx <dragonflies@gmail.com>
2026-02-25 10:34:44 -0500

    feat(hosts): Switch to nixos user and set static IDs

--- hosts/launch-octopus/default.nix
+++ hosts/launch-octopus/default.nix
@@ -17,17 +17,17 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.cael = {
+  users.users.nixos = {
     isNormalUser = true;
     uid = 1000;
-    description = "cael";
+    description = "nixos";
     extraGroups = [ "wheel" ];
     packages = with pkgs; [
       # User specific packages can also go here
     ];
   };
 
-  users.groups.cael.gid = 1000;
+  users.groups.nixos.gid = 1000;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
