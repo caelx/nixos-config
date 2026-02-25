632ea3f4154e2dd88663b25393e89c30fac8fae9
caelx <dragonflies@gmail.com>
2026-02-25 10:07:29 -0500

    feat(flake): Expose workstation host and integrate home-manager

--- flake.nix
+++ flake.nix
@@ -2,19 +2,15 @@
   description = "Unified NixOS Configuration Repository";
 
   inputs = {
-    # NixOS official package source, using the nixos-24.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
-    # Home Manager for user-level configuration
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Optimized hardware configurations
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-    # Secrets management
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -22,16 +18,22 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    # NixOS configurations
     nixosConfigurations = {
-      # Placeholder for the first host
-      # workstation = nixpkgs.lib.nixosSystem { ... };
-    };
-
-    # Home Manager configurations
-    homeConfigurations = {
-      # Placeholder for the primary user
-      # "james" = home-manager.lib.homeManagerConfiguration { ... };
+      workstation = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/workstation/default.nix
+          
+          # Integrate home-manager
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.cael = import ./home/cael.nix;
+            # home-manager.extraSpecialArgs = { inherit inputs; };
+          }
+        ];
+      };
     };
   };
 }
