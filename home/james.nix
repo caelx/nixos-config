b8a086c2861d3d1a1ccc7a3a37a6db45929a4045
caelx <dragonflies@gmail.com>
2026-02-25 10:05:55 -0500

    feat(home): Add base home-manager configuration for james

---
+++ home/james.nix
@@ -0,0 +1,59 @@
+{ pkgs, ... }:
+
+{
+  home.username = "james";
+  home.homeDirectory = "/home/james";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # Add user-specific packages here
+    bat
+    eza
+    fd
+    ripgrep
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+  };
+
+  # You can also manage environment variables through 'home.sessionVariables'.
+  # If you don't want to manage your shell through Home Manager, then you
+  # have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/james/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Git configuration
+  programs.git = {
+    enable = true;
+    userName = "James";
+    # userEmail = "james@example.com"; # User should override
+  };
+}
