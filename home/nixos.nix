8f6120c2dd9636c6484fb01a483184b2f0c50da8
caelx <dragonflies@gmail.com>
2026-02-26 19:54:06 +0000

    fix(ssh): move consolidated options to extraOptions in matchBlocks

--- home/nixos.nix
+++ home/nixos.nix
@@ -204,18 +204,18 @@
       "*" = {
         user = "cael";
         identityFile = "~/.ssh/id_ed25519";
-        forwardAgent = true;
-        addKeysToAgent = "yes";
-        compression = true;
-        serverAliveInterval = 60;
-        serverAliveCountMax = 30;
-        controlMaster = "auto";
-        controlPath = "~/.ssh/+%h-%p-%r";
-        controlPersist = "5m";
         extraOptions = {
-          "StrictHostKeyChecking" = "accept-new";
+          "AddKeysToAgent" = "yes";
+          "ForwardAgent" = "yes";
+          "Compression" = "yes";
+          "ServerAliveInterval" = "60";
+          "ServerAliveCountMax" = "30";
           "HashKnownHosts" = "no";
           "UserKnownHostsFile" = "~/.ssh/known_hosts";
+          "ControlMaster" = "auto";
+          "ControlPath" = "~/.ssh/+%h-%p-%r";
+          "ControlPersist" = "5m";
+          "StrictHostKeyChecking" = "accept-new";
         };
       };
     };
