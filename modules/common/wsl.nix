c8eb7486397734db57cd86b2c8c037d92db50a02
caelx <dragonflies@gmail.com>
2026-02-27 17:38:58 +0000

    feat(wsl): Integrate notify-send bridge as a custom NixOS package

--- modules/common/wsl.nix
+++ modules/common/wsl.nix
@@ -27,6 +27,64 @@
     # Allows opening files and directories in Windows applications
     # e.g., 'wsl-open .' opens the current folder in Windows Explorer
     pkgs.wsl-open
+
+    # WSL notify-send Bridge
+    (pkgs.writeShellScriptBin "notify-send" ''
+      # WSL notify-send Bridge (Forward to Windows)
+      # Hardcoded to use Windows Terminal icon for branding.
+      
+      APP_NAME="WSL"
+      SUMMARY=""
+      BODY=""
+
+      # Parse arguments
+      while [[ $# -gt 0 ]]; do
+          case "$1" in
+              -a|--app-name) APP_NAME="$2"; shift 2 ;;
+              --app-name=*) APP_NAME="''${1#*=}"; shift ;;
+              -u|--urgency|--urgency=*) shift 2 ;; # Ignore urgency
+              -t|--expire-time|--expire-time=*) shift 2 ;; # Ignore timeout
+              -i|--icon|--icon=*) shift 2 ;; # Ignore icon
+              -h|--help)
+                  echo "Usage: notify-send [OPTIONS] <SUMMARY> [BODY]"
+                  exit 0
+                  ;;
+              *)
+                  if [[ "$1" == -* ]]; then shift
+                  elif [ -z "$SUMMARY" ]; then SUMMARY="$1"; shift
+                  elif [ -z "$BODY" ]; then BODY="$1"; shift
+                  else shift; fi
+                  ;;
+          esac
+      done
+
+      [ -z "$SUMMARY" ] && exit 1
+
+      # Escape single quotes for PowerShell
+      SUMMARY_ESCAPED=$(echo "$SUMMARY" | sed "s/'/''/g")
+      BODY_ESCAPED=$(echo "$BODY" | sed "s/'/''/g")
+      APP_NAME_ESCAPED=$(echo "$APP_NAME" | sed "s/'/''/g")
+
+      # Invoke PowerShell to show the toast
+      /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "
+          [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > \$null
+          \$wtPackage = Get-AppxPackage -Name Microsoft.WindowsTerminal
+          if (\$wtPackage) { \$iconPath = Join-Path \$wtPackage.InstallLocation 'Images\Square44x44Logo.targetsize-256.png' }
+          \$Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastImageAndText02)
+          \$RawXml = [xml]\$Template.GetXml()
+          (\$RawXml.toast.visual.binding.text | Where-Object { \$_.id -eq '1' }).AppendChild(\$RawXml.CreateTextNode('$SUMMARY_ESCAPED')) > \$null
+          (\$RawXml.toast.visual.binding.text | Where-Object { \$_.id -eq '2' }).AppendChild(\$RawXml.CreateTextNode('$BODY_ESCAPED')) > \$null
+          if (\$iconPath -and (Test-Path \$iconPath)) {
+              \$imageNode = (\$RawXml.toast.visual.binding.image | Where-Object { \$_.id -eq '1' })
+              if (\$imageNode) { \$imageNode.SetAttribute('src', \$iconPath) }
+          }
+          \$SerializedXml = [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]::New()
+          \$SerializedXml.LoadXml(\$RawXml.OuterXml)
+          \$Toast = [Windows.UI.Notifications.ToastNotification, Windows.UI.Notifications, ContentType = WindowsRuntime]::New(\$SerializedXml)
+          \$Toast.Tag = '$APP_NAME_ESCAPED'; \$Toast.Group = 'WSL'
+          [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('PowerShell').Show(\$Toast)
+      "
+    '')
   ];
 
   # Activation script to create ~/win-home symlink for the nixos user
