9f2240904a0e8d411c57699e500acfa5426d1785
caelx <dragonflies@gmail.com>
2026-03-28 00:12:28 +0000

    fix(chill-penguin): allow Cloudflare SSH MACs

--- hosts/chill-penguin/default.nix
+++ hosts/chill-penguin/default.nix
@@ -32,6 +32,13 @@ in
   services.openssh = {
     enable = true;
     settings.PermitRootLogin = "prohibit-password";
+    macs = [
+      "hmac-sha2-256"
+      "hmac-sha2-512"
+      "hmac-sha2-256-etm@openssh.com"
+      "hmac-sha2-512-etm@openssh.com"
+      "umac-128-etm@openssh.com"
+    ];
   };
 
   # NFS support
