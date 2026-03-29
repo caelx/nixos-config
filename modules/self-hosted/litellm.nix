19a681406900799c656cf30986a8013351d8f7c4
caelx <dragonflies@gmail.com>
2026-03-29 05:46:15 +0000

    feat(self-hosted): use LITELLM_CONFIG_JSON to trigger DB features

--- modules/self-hosted/litellm.nix
+++ modules/self-hosted/litellm.nix
@@ -10,11 +10,16 @@
       LITELLM_LOG = "DEBUG";
       STORE_MODEL_IN_DB = "True";
       USE_PRISMA_MIGRATE = "True";
+      # USE_PRISMA_DB_PUSH = "True";
+      
       # Production best practices
       PROXY_BATCH_WRITE_AT = "60";
       DATABASE_CONNECTION_POOL_LIMIT = "10";
       ALLOW_REQUESTS_ON_DB_UNAVAILABLE = "True";
       DISABLE_LOAD_DOTENV = "True";
+
+      # Use a minimal JSON config to trigger features if env vars alone aren't enough
+      LITELLM_CONFIG_JSON = ''{"model_list": [], "litellm_settings": {"drop_params": true, "set_verbose": true}}'';
     };
     environmentFiles = [
       config.sops.secrets."litellm-secrets".path
