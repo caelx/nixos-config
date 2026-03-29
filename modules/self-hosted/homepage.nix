18f3c5a1558fa24962bf0ddef45482c26e72fdd0
caelx <dragonflies@gmail.com>
2026-03-29 04:54:49 +0000

    style(self-hosted): use color (sh-) icons for CloakBrowser and LiteLLM

--- modules/self-hosted/homepage.nix
+++ modules/self-hosted/homepage.nix
@@ -182,12 +182,12 @@ in
             "[Downloads].[VueTorrent].widget.url=literal:http://gluetun:5000"
             
             # Services group
-            "[Services].[CloakBrowser].icon=literal:mdi-google-chrome"
+            "[Services].[CloakBrowser].icon=literal:sh-google-chrome"
             "[Services].[CloakBrowser].description=literal:Stealth Browser Manager"
             "[Services].[CloakBrowser].server=literal:chill-penguin"
             "[Services].[CloakBrowser].container=literal:cloakbrowser"
 
-            "[Services].[LiteLLM].icon=literal:mdi-train"
+            "[Services].[LiteLLM].icon=literal:sh-openai"
             "[Services].[LiteLLM].description=literal:LLM Gateway"
             "[Services].[LiteLLM].server=literal:chill-penguin"
             "[Services].[LiteLLM].container=literal:litellm"
