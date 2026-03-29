c4d471bc6e11bb96f17405b7e5b79744ae4af4fa
caelx <dragonflies@gmail.com>
2026-03-29 07:24:57 +0000

    chore(cleanup): remove old test files and unused hermes module

--- cloak-test-tmp/strip-origin.py
+++
@@ -1,8 +0,0 @@
-from mitmproxy import http
-
-def request(flow: http.HTTPFlow) -> None:
-    # Remove Origin headers to prevent CSWSH block
-    if "Origin" in flow.request.headers:
-        del flow.request.headers["Origin"]
-    if "origin" in flow.request.headers:
-        del flow.request.headers["origin"]
