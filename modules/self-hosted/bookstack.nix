df923e42d446001d6a07a768ceb207caaab08ac0
caelx <dragonflies@gmail.com>
2026-04-12 12:53:58 +0000

    fix(self-hosted): run bookstack as root

--- modules/self-hosted/bookstack.nix
+++ modules/self-hosted/bookstack.nix
@@ -10,7 +10,6 @@ in
     labels = {
       "io.containers.autoupdate" = "registry";
     };
-    user = "3000:3000";
     extraOptions = [
       "--network=ghostship_net"
       "--health-cmd=wget -q --spider --tries=1 --timeout=5 http://127.0.0.1/ || exit 1"
