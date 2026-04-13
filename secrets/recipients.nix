4a3bd8ab48023571a27ffc6b0a1e10dc916ac7f5
chill-penguin <nixos@chill-penguin>
2026-04-13 11:00:14 +0000

    fix(secrets): add armored-armadillo ragenix key

--- secrets/recipients.nix
+++ secrets/recipients.nix
@@ -1,11 +1,13 @@
 let
   operatorEditKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+8CwidWx8nvTrjzJnvcS0Y5xcAu/fpQnwh6YcShlg/ nixos@launch-octopus";
   chillPenguinHostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM/T11y26OiGS9bWD3QaBY3Cfgt+KM/V5351E6saTOUG root@chill-penguin";
+  armoredArmadilloEditKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPCsSQVs2eDV+wkfkARTRUoPz7og9Zcfo8oKj4u6oBvv nixos@armored-armadillo-ragenix";
 in
 rec {
   keys = {
     operators = {
       ragenix = operatorEditKey;
+      armored-armadillo = armoredArmadilloEditKey;
     };
 
     hosts = {
@@ -14,7 +16,7 @@ rec {
   };
 
   groups = {
-    editors = [ keys.operators.ragenix ];
+    editors = [ keys.operators.ragenix keys.operators.armored-armadillo ];
     self-hosted-runtime = groups.editors ++ [ keys.hosts.chill-penguin ];
   };
 }
