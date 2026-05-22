ae370b86c08fd039f07bad243184ab811bd4f68e
caelx <dragonflies@gmail.com>
2026-05-22 00:39:24 +0000

    secrets: add armored armadillo recipient

--- secrets/recipients.nix
+++ secrets/recipients.nix
@@ -2,6 +2,7 @@ let
   operatorEditKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+8CwidWx8nvTrjzJnvcS0Y5xcAu/fpQnwh6YcShlg/ nixos@launch-octopus";
   chillPenguinHostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM/T11y26OiGS9bWD3QaBY3Cfgt+KM/V5351E6saTOUG root@chill-penguin";
   boomerKuwangerHostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILT2HGF6WHU2N5BdeVcxmTAu98b8Rpc9ddOx3FXaB179 root@boomer-kuwanger";
+  armoredArmadilloHostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPrrP0rcJAYANZ+xZNPcuKrFhSZkLKuqlukoE1x1Ei+G root@armored-armadillo";
   armoredArmadilloEditKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPCsSQVs2eDV+wkfkARTRUoPz7og9Zcfo8oKj4u6oBvv nixos@armored-armadillo-ragenix";
 in
 rec {
@@ -12,6 +13,7 @@ rec {
     };
 
     hosts = {
+      armored-armadillo = armoredArmadilloHostKey;
       chill-penguin = chillPenguinHostKey;
       boomer-kuwanger = boomerKuwangerHostKey;
     };
@@ -22,9 +24,13 @@ rec {
       keys.operators.ragenix
       keys.operators.armored-armadillo
     ];
-    self-hosted-runtime = groups.editors ++ [ keys.hosts.chill-penguin ];
+    self-hosted-runtime = groups.editors ++ [
+      keys.hosts.armored-armadillo
+      keys.hosts.chill-penguin
+    ];
     emulation-runtime = groups.editors ++ [ keys.hosts.boomer-kuwanger ];
     shared-runtime = groups.editors ++ [
+      keys.hosts.armored-armadillo
       keys.hosts.chill-penguin
       keys.hosts.boomer-kuwanger
     ];
