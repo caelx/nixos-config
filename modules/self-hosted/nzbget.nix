3c3d2f41f03eedd43f9fac4db70e4229675c76e1
caelx <dragonflies@gmail.com>
2026-08-17 23:38:04 +0000

    nzbget: move to ghostship_net and update homepage endpoint

--- modules/self-hosted/nzbget.nix
+++ modules/self-hosted/nzbget.nix
@@ -14,7 +14,7 @@
     };
     user = "3000:3000";
     extraOptions = [
-      "--network=container:gluetun"
+      "--network=ghostship_net"
       "--health-cmd=wget -q --spider --tries=1 --timeout=5 http://127.0.0.1:5001/ || exit 1"
       "--health-interval=30s"
       "--health-timeout=10s"
@@ -38,13 +38,13 @@
 
   systemd.services.podman-nzbget = {
     after = [
+      "network-online.target"
       "mnt-share.mount"
-      "podman-gluetun.service"
     ];
-    bindsTo = [ "podman-gluetun.service" ];
-    partOf = [ "podman-gluetun.service" ];
-    requires = [ "podman-gluetun.service" ];
-    wants = [ "mnt-share.mount" ];
+    wants = [
+      "network-online.target"
+      "mnt-share.mount"
+    ];
   };
 
   systemd.tmpfiles.rules = [
